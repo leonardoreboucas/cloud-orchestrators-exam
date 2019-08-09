69a34d5a9adc2c1eccb7241f9181dfd68fcce54b
Leonardo Carvalho <leonardo_r_carvalho@yahoo.com>
2019-08-09 01:15:36 -0300

    ad

--- play_test.sh
+++ play_test.sh
@@ -12,7 +12,7 @@ list_providers='gcp azure'
 # Virginia / London / São Pauol
 list_aws_region='us-east-1 eu-west-2 sa-east-1'
 list_gcp_region='us-east-4 europe-west-2 southamerica-east-1'
-list_azure_region='East+US UK+South Brazil+South'
+list_azure_region='East_US UK_South Brazil_South'
 
 ########## Executions ##########
 test_executions=3
@@ -44,10 +44,9 @@ while [ $execution -lt $test_executions ]; do
           cfy deployment delete $i
           cfy blueprints delete cloudify-wordpress-blueprint-$i
         done
-        region=$(echo $region | sed -e 's/+/ /g')
         case $orchestrator in
           cloudify)
-            INPUTS="${provider}_region_name='${region}'"
+            INPUTS="${provider}_region_name='$(echo $region | sed -e 's/_/ /g')'"
             cmd_provision="cfy install -b cloudify-wordpress-blueprint-${provider} -d $provider -i ${INPUTS} cloudify/${provider}.yaml"
             cmd_unprovision="cfy uninstall -f -v -p ignore_failure=true ${provider}"
             ;;
@@ -58,14 +57,14 @@ while [ $execution -lt $test_executions ]; do
           *)
         esac
         echo -e "\n${orchestrator} - ${provider} - ${region} - ${execution}"
-        LOCAL=executions/${EXEC_DATE}/${orchestrator}/${provider}/"${region}"/${execution}
+        LOCAL=executions/${EXEC_DATE}/${orchestrator}/${provider}/${region}/${execution}
         mkdir -p $LOCAL
-        test_monitor.sh ${EXEC_DATE} provision ${orchestrator} ${provider} "${region}" ${execution} &
+        test_monitor.sh ${EXEC_DATE} provision ${orchestrator} ${provider} ${region} ${execution} &
         monitor_pid=$!
         $cmd_provision >> $LOCAL/provision.log
         kill -9 $monitor_pid &> /dev/null
         sleep 30
-        test_monitor.sh ${EXEC_DATE} unprovision ${orchestrator} ${provider} "${region}" ${execution} &
+        test_monitor.sh ${EXEC_DATE} unprovision ${orchestrator} ${provider} ${region} ${execution} &
         monitor_pid=$!
         $cmd_unprovision >> $LOCAL/unprovision.log
         kill -9 $monitor_pid &> /dev/null
