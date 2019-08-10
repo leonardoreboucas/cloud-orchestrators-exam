812e1e29f505bb2517decf9700c895c2f5299c0c
Leonardo Carvalho <leonardo_r_carvalho@yahoo.com>
2019-08-10 16:05:39 -0300

    add

--- terraform/test/commands.sh
+++ terraform/test/commands.sh
@@ -1,3 +1,3 @@
 DIR_BASE=${DIR}/${orchestrator}/${provider}
-cmd_provision="cd ${DIR_BASE} ; terraform apply --auto-approve"
-cmd_unprovision="terraform destroy --auto-approve ; cd -"
+cmd_provision="cd ${DIR_BASE} && terraform apply --auto-approve"
+cmd_unprovision="terraform destroy --auto-approve && cd -"
