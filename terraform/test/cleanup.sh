e326341d3e66b39c396eda7a8d94e5acfd8b4b9c
Leonardo Carvalho <leonardo_r_carvalho@yahoo.com>
2019-08-10 15:16:15 -0300

    add

--- terraform/test/cleanup.sh
+++ terraform/test/cleanup.sh
@@ -1,6 +1,7 @@
 echo "Terraform Init..."
 for provider in $list_providers; do
   echo "(${provider})..."
-  terraform init ${orchestrator}/${provider}
-  terraform destroy --auto-approve ${orchestrator}/${provider}
+  DIR_BASE=${DIR}/${orchestrator}/${provider}
+  terraform init -plugin-dir ${DIR_BASE} ${DIR_BASE}
+  terraform destroy --auto-approve --backup=$DIR_BASE --state=$DIR_BASE --state-out=$DIR_BASE $DIR_BASE
 done
--- terraform/test/commands.sh
+++ terraform/test/commands.sh
@@ -1,4 +1,4 @@
 DIR_BASE=${DIR}/${orchestrator}/${provider}
 cmd_provision="terraform apply --var region_name=${region} --var availability_zone=$DIR_BASE --auto-approve --backup=$DIR_BASE --state=$DIR_BASE --state-out=$DIR_BASE $DIR_BASE"
-cmd_unprovision="terraform destroy --auto-approve --auto-approve --backup=$DIR_BASE --state=$DIR_BASE --state-out=$DIR_BASE $DIR_BASE"
+cmd_unprovision="terraform destroy --auto-approve --backup=$DIR_BASE --state=$DIR_BASE --state-out=$DIR_BASE $DIR_BASE"
 unset DIR_BASE
