e24a8d3571c4a0a5e83b42651bb0e0bfdfec6b7c
Leonardo Carvalho <leonardo_r_carvalho@yahoo.com>
2019-08-10 17:06:45 -0300

    add

--- play_test.sh
+++ play_test.sh
@@ -17,7 +17,7 @@ list_azure_region='EastUS UKSouth BrazilSouth'
 test_executions=1
 
 echo "Starting tests..."
-EXEC_DATE=$(date +%Y-%m-%d-%H-%i-%s)
+EXEC_DATE=$(date +%Y-%m-%d-%H-%M-%S)
 mkdir -p ${DIR}/executions/${EXEC_DATE}
 RESULTS=${DIR}/executions/${EXEC_DATE}/results
 echo "date,process,orchestrator,provider,region,execution,timestamp,cpu,mem,io,net,duration" > ${RESULTS}
