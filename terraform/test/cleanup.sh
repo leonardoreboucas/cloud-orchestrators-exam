1ec9e4c3ec2556fa883c1342d3933c76e31e8278
Leonardo Carvalho <leonardo_r_carvalho@yahoo.com>
2019-08-10 14:39:55 -0300

    add

--- terraform/test/cleanup.sh
+++ terraform/test/cleanup.sh
@@ -1,6 +1,6 @@
 echo "Terraform Init..."
-for i in $list_providers; do
-  echo "($i)..."
+for provider in $list_providers; do
+  echo "(${provider})..."
   terraform init ${orchestrator}/${provider}
   terraform destroy --auto-approve ${orchestrator}/${provider}
 done
