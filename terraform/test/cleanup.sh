59ee94df4aaf9c95160efaff778222c8325522d1
Leonardo Carvalho <leonardo_r_carvalho@yahoo.com>
2019-08-10 15:24:10 -0300

    add

--- terraform/test/cleanup.sh
+++ terraform/test/cleanup.sh
@@ -2,6 +2,6 @@ echo "Terraform Init..."
 for provider in $list_providers; do
   echo "(${provider})..."
   DIR_BASE=${DIR}/${orchestrator}/${provider}
-  terraform init -plugin-dir ${DIR_BASE}/.terraform/plugins ${DIR_BASE}
-  #terraform destroy --auto-approve --backup=$DIR_BASE --state=$DIR_BASE --state-out=$DIR_BASE $DIR_BASE
+  terraform init --get-plugins --plugin-dir=${DIR_BASE}/.terraform/plugins ${DIR_BASE}
+  terraform destroy --auto-approve --backup=$DIR_BASE --state=$DIR_BASE --state-out=$DIR_BASE $DIR_BASE
 done
