eb18bae1088f15008d21a275d76d7a212418f17e
Leonardo Carvalho <leonardo_r_carvalho@yahoo.com>
2019-08-10 18:16:08 -0300

    add

--- terraform/test/commands.sh
+++ terraform/test/commands.sh
@@ -1,2 +1,2 @@
 cmd_provision="terraform apply --auto-approve -var region_name=${region} --var availability_zone=${region}${zone}"
-cmd_unprovision="terraform destroy --auto-approve --refresh=true  -var region_name=${region} --var availability_zone=${region}${zone}"
+cmd_unprovision="terraform destroy --auto-approve --refresh=true"
