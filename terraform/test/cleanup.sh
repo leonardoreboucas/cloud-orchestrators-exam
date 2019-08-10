4d8e3ea6bf7a86fc94d32ec9b9355aec7772220c
Leonardo Carvalho <leonardo_r_carvalho@yahoo.com>
2019-08-10 15:33:23 -0300

    add

--- terraform/test/cleanup.sh
+++ terraform/test/cleanup.sh
@@ -3,5 +3,6 @@ for provider in $list_providers; do
   echo "(${provider})..."
   DIR_BASE=${DIR}/${orchestrator}/${provider}
   terraform init ${DIR_BASE}
-  terraform destroy --auto-approve --backup=$DIR_BASE --state=$DIR_BASE --state-out=$DIR_BASE $DIR_BASE
+  terraform destroy --auto-approve --backup=$DIR_BASE/.tf.backup --state=$DIR_BASE/.tf.state --state-out=$DIR_BASE/.tf.state-out $DIR_BASE
+  rm -rf $DIR_BASE/.tf.*
 done
--- terraform/test/commands.sh
+++ terraform/test/commands.sh
@@ -1,4 +1,4 @@
 DIR_BASE=${DIR}/${orchestrator}/${provider}
-cmd_provision="terraform apply --var region_name=${region} --var availability_zone=$DIR_BASE --auto-approve --backup=$DIR_BASE --state=$DIR_BASE --state-out=$DIR_BASE $DIR_BASE"
-cmd_unprovision="terraform destroy --auto-approve --backup=$DIR_BASE --state=$DIR_BASE --state-out=$DIR_BASE $DIR_BASE"
+cmd_provision="terraform apply --var region_name=${region} --var availability_zone=$DIR_BASE --auto-approve --backup=$DIR_BASE/.tf.backup --state=$DIR_BASE/.tf.state --state-out=$DIR_BASE/.tf.state-out $DIR_BASE"
+cmd_unprovision="terraform destroy --auto-approve --backup=$DIR_BASE/.tf.backup --state=$DIR_BASE/.tf.state --state-out=$DIR_BASE/.tf.state-out $DIR_BASE"
 unset DIR_BASE
