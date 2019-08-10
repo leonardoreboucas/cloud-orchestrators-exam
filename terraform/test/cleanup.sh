2536796d51392e9b98dc73dc69feed66ddf27bda
Leonardo Carvalho <leonardo_r_carvalho@yahoo.com>
2019-08-10 17:17:34 -0300

    add

--- terraform/test/cleanup.sh
+++ terraform/test/cleanup.sh
@@ -3,7 +3,8 @@ for provider in $list_providers; do
   echo "(${provider})..."
   DIR_BASE=${DIR}/${orchestrator}/${provider}
   cd $DIR_BASE
-  terraform init ${DIR_BASE}
+  terraform init
+  terraform refresh
   terraform destroy --auto-approve
   cd -
 done
