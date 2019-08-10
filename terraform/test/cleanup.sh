70b1c9936874c1955e7927ce1912b2527ae05949
Leonardo Carvalho <leonardo_r_carvalho@yahoo.com>
2019-08-10 16:01:05 -0300

    add

--- terraform/test/cleanup.sh
+++ terraform/test/cleanup.sh
@@ -3,8 +3,7 @@ for provider in $list_providers; do
   echo "(${provider})..."
   DIR_BASE=${DIR}/${orchestrator}/${provider}
   cd $DIR_BASE
-  pwd
   terraform init ${DIR_BASE}
-  terraform destroy --auto-approve $DIR_BASE
+  terraform destroy --auto-approve
   cd -
 done
--- terraform/test/commands.sh
+++ terraform/test/commands.sh
@@ -1,7 +1,5 @@
 DIR_BASE=${DIR}/${orchestrator}/${provider}
 cd $DIR_BASE
-pwd
 cmd_provision="terraform apply --auto-approve"
 cmd_unprovision="terraform destroy --auto-approve"
 cd -
-unset DIR_BASE
