9833a0d170a8dc06c5c6623f6ad7317009aee1a0
Leonardo Carvalho <leonardo_r_carvalho@yahoo.com>
2019-08-10 22:20:09 -0300

    add

--- cloudify/test/commands.sh
+++ cloudify/test/commands.sh
@@ -1,4 +1,4 @@
 INPUTS="${provider}_region_name=$region"
 BPID="$(date +%s)"
-cmd_provision="cfy install -b ${BPID}-cloudify-wp-${provider} -d $provider -i ${INPUTS} ${DIR}/cloudify/${provider}.yaml"
+cmd_provision="cfy install -b cloudify-${BPID}-wp-${provider} -d $provider -i ${INPUTS} ${DIR}/cloudify/${provider}.yaml"
 cmd_unprovision="cfy uninstall -f -v -p ignore_failure=true ${provider}"
