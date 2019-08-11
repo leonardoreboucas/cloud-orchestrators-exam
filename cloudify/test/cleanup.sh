52cf21c37148e67db1d8a1067bd3ace6eea56b78
Leonardo Carvalho <leonardo_r_carvalho@yahoo.com>
2019-08-10 21:59:22 -0300

    add

--- cloudify/test/cleanup.sh
+++ cloudify/test/cleanup.sh
@@ -3,5 +3,5 @@ for i in $list_providers; do
   echo "Cleaning Cloudify Manager ($i)..."
   cfy uninstall -f -v -p ignore_failure=true $i
   cfy deployment delete $i
-  cfy blueprints delete cloudify-wordpress-blueprint-$i
+  cfy blueprints delete cloudify-wp-blueprint-$i
 done
--- cloudify/test/commands.sh
+++ cloudify/test/commands.sh
@@ -1,3 +1,3 @@
 INPUTS="${provider}_region_name=$region"
-cmd_provision="cfy install -b cloudify-wordpress-blueprint-${provider} -d $provider -i ${INPUTS} ${DIR}/cloudify/${provider}.yaml"
+cmd_provision="cfy install -b cloudify-wp-${provider} -d $provider -i ${INPUTS} ${DIR}/cloudify/${provider}.yaml"
 cmd_unprovision="cfy uninstall -f -v -p ignore_failure=true ${provider}"
